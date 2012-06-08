6840deef7425533b3d6f0af8eb62f42c0a80a8a2
Ann Paul <annp89@algoviz-beta.cc.vt.edu>
2012-06-07 21:38:58 -0400

    Updated index.php

--- index.php
+++ index.php
@@ -1,30 +1,188 @@
-<?php
-$myFile = "/home/algoviz-beta/QBank/QBank/NewFile2.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
-$stringData = $_POST['question_text'];
-fwrite($fh, $stringData);
-fclose($fh);
-?>
 
 
-<?php
-	$a="hello";
-
-?>
-
-
+<!DOCTYPE html>
 <html>
+<head>
+    <title>QBank: Question Bank generation made easy.</title>
+  
+  <meta name="Description" content="A Question generation tool to create parameterized questions.">
+  <link href="new.css" rel="stylesheet" type="text/css" />
+<table border ="1" width="100%">
+<tr>
+  <td border="1" align="center" width="10%"><a href="index.html"><img src="QBank.png" href = /></a></td>
+  <td align="left width="90%"><h1>QBank - Question banking made easy with parameterization. </h1></td>
+</tr>
+</table>
 
-Finally my php code is working!!!!!!!!!!
+
+</head>
+
+<body>
+   <p>
+	<p>
+   <div id="container">
+      
+      <div id="header">
+
+        <a href="/" class="banner1"></a>
+                
+
+      <div id="attention">
+        
+                  
+
+      </div>
+   
+      <div id="main">
+         
+<div style="font-size:16px; float:center">
+<table border="1" width="100%" height = "400px"> 
+
+  <div style=" clear:both ">
+  <tr>  
+	 <div style="clear:both; float:right">
+      	<td width="50%"> <div class="infoBox">
+         <h2>Create Question</h2>
+         Contribute a parameterized question in any subject area!
+        	 <div style="font-size:20px; padding-top: 10px;">
+           	<a href="editQuestion.html">&gt;&gt; Write a question!</a>
+		 </div>
+       </div>
+</td>
+<td width="50%">
 
 
-<?php
-$myFile = "/home/algoviz-beta/QBank/QBank/abc.txt";
-$fh = fopen($myFile, 'w');
-$stringData = "HEy";
-fwrite($fh, $stringData);
-fclose($fh);
+<div class="infoBox">
+         <h2>Cluster Simple => Group questions</h2>
+         Create Group questions from exiting Simple questions
+         <div style="font-size:20px; padding-top: 10px">
+           <a href="ClusSearchQ.php">&gt;&gt; Cluster Exercises</a>
+         </div>
+       </div>
+       
+</td>
+	</div>
+  </tr>
+<tr>
+	<div style="clear:both">
+       <td><div class="infoBox">
+         <h2>View and Delete a question</h2>
+         View and delete a question
+         <div style="font-size:20px; padding-top: 10px">
+           <a href="viewQ.php">&gt;&gt;View and Delete a question!</a>
+         </div>
+       </div>
+</td>
+<td>
+       <div class="infoBox">
+         <h2>KhanAcademy exercises</h2>
+         View created questions in Khan Acadedmy 
+         <div style="font-size:20px; padding-top: 10px">
+           <a href="KAsearchQ.php">&gt;&gt; Khan Academy Exercises</a>
+         </div>
+       
+</td>
+</tr>
+</div>
+<tr>
+<div style="clear:both">
+<td>
+       <div class="infoBox">
+         <h2>Find and Edit question</h2>
+         Search for and edit existing questions.
+         	<div style="font-size:20px; padding-top: 10px">
+          	 <a href="searchQ.php">&gt;&gt; Search and edit a question!</a>
+		</div>
+       </div>
+</td>
+     </div>
+</tr>
+    </div>
+</table>
+  </div>
 
-?>
+
+  <style type="text/css">
+    #homeCarousel {
+      border: 1px solid rgb(0, 0, 0); 
+      visibility: visible; 
+      overflow: hidden; 
+      position: relative; 
+      z-index: 2; 
+      width: 300px; 
+      height: 400px;
+    }
+    #homeCarousel ul {
+      margin: 0pt; 
+      padding: 0pt; 
+      position: relative; 
+      list-style-type: none; 
+      z-index: 1; 
+      left: 10px;
+    }
+    div.carouselQuestion {
+      margin: 10pt; 
+      padding: 0pt; 
+      position: relative; 
+      list-style-type: none; 
+      z-index: 3; 
+      width: 300px; 
+      height: 500px; 
+      left:0px; 
+      cursor: pointer;
+    }
+  </style>
+<!--
+  <div id="homeCarousel" class="carousel" style="float:center">
+	  <ul>
+  		<li>
+  			<div class="quad-question carouselQuestion" style="float:center">
+    			
+   
+
+
+<div class="quad-question_content">
+  <p>How is data entered in a queue? </p>
+</div>
+
+   <div class="quad-answer_choices">
+      <table>
+         <tr>
+            <td class="quad-answer_choice_index" >
+               <p>a)</p>
+            </td>
+            <td class="quad-answer_choice" >
+               <p>Last In First Out</p>
+            </td>
+         </tr>
+         <tr>
+            <td class="quad-answer_choice_index" >
+               <p>b)</p>
+            </td>
+            <td class="quad-answer_choice" >
+               <p>First In First Out</p>
+            </td>
+         </tr>
+		 <tr>
+          <td class="quad-answer_choice_index" >
+               <p>c)</p>
+            </td>
+            <td class="quad-answer_choice">
+               <p>None of the above</p>
+            </td>
+         </tr>
+      </table>  
+   </div>
+</div>
+</div>
+-->
+   <p></p>
+
+    <div id="footer" style="float:center">
+     	  <a href="index.html">Features</a> | 
+         <a href="index.html">About QBank</a> | 
+         <a href="index.html">Contact Us</a>
+      </div>
+
 
 </html>
